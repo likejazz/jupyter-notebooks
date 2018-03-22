6c14692a294e3526b7fed74d06618ae48f5d0f4c
Sang-Kil Park <kaon.park@kakaocorp.com>
2018-03-23 00:16:39 +0900

    Clean up files.

--- deep-learning/linear-regression-tensorflow.py
+++ deep-learning/linear-regression-tensorflow.py
@@ -2,16 +2,16 @@ import tensorflow as tf
 import numpy as np
 
 X = tf.placeholder(tf.float32, name="X")
-x_data = np.array([[1,1,4,5,6,8,9]], dtype=np.float)
+x_data = np.array([[1, 1, 4, 5, 6, 8, 9]], dtype=np.float)
 x_data = x_data.reshape(-1, 1)
 
 t = tf.placeholder(tf.float32, name="t")
-t_data = np.array([[107],[83],[70]], dtype=np.float)
+t_data = np.array([[107], [83], [70]], dtype=np.float)
 
-W = tf.Variable([[1,5,3,7,4,5,66], [2,4,23,1,2,1,1], [4,4,1,1,1,1,2]], dtype=tf.float32)
+W = tf.Variable([[1, 5, 3, 7, 4, 5, 66], [2, 4, 23, 1, 2, 1, 1], [4, 4, 1, 1, 1, 1, 2]], dtype=tf.float32)
 y = tf.matmul(W, X)
 
-cost = tf.reduce_sum(1/2 * tf.square(y - t))
+cost = tf.reduce_sum(1 / 2 * tf.square(y - t))
 
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(cost)
 
