9bbfdc762cddda2997e64bdebbde0752e2eb4f8b
Sang-Kil Park <kaon.park@kakaocorp.com>
2018-04-26 01:30:19 +0900

    .

---
+++ data-science/prob-with-permutations.py
@@ -0,0 +1,26 @@
+# %%
+from random import randint
+
+
+def gen_str(length=10):
+    alphanum = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    str = []
+    for i in range(length):
+        str.append(alphanum[randint(0, len(alphanum) - 1)])
+
+    return ''.join(str)
+
+
+sentence = gen_str(10)
+
+total_trials = 1000000
+pattern_length = 3
+matched_count = 0
+for i in range(total_trials):
+    pattern = gen_str(pattern_length)
+    if pattern in sentence:
+        matched_count += 1
+
+# Print out probability with permutations.
+print("%d/%d %f" % (matched_count, total_trials, matched_count / total_trials))
