2d6ea81f9e66e5b3cf885b3ca245ff8419976e46
Sang-Kil Park <kaon.park@kakaocorp.com>
2018-06-19 18:29:26 +0900

    .

--- deep-learning/addition_rnn.py
+++ deep-learning/addition_rnn.py
@@ -160,7 +160,6 @@ print(y_val.shape)
 RNN = layers.LSTM
 HIDDEN_SIZE = 128
 BATCH_SIZE = 512
-LAYERS = 1
 
 # %%
 print('Build model...')
@@ -173,17 +172,13 @@ model.add(RNN(HIDDEN_SIZE, input_shape=(MAXLEN, len(chars))))
 # RNN for each time step. Repeat 'DIGITS + 1' times as that's the maximum
 # length of output, e.g., when DIGITS=3, max output is 999+999=1998.
 model.add(layers.RepeatVector(DIGITS + 1))
-# The decoder RNN could be multiple layers stacked or a single layer.
-for _ in range(LAYERS):
-    # By setting return_sequences to True, return not only the last output but
-    # all the outputs so far in the form of (num_samples, timesteps,
-    # output_dim). This is necessary as TimeDistributed in the below expects
-    # the first dimension to be the timesteps.
-    model.add(RNN(HIDDEN_SIZE, return_sequences=True))
+model.add(RNN(HIDDEN_SIZE, return_sequences=True))
+# model.add(layers.Dense(HIDDEN_SIZE))
 
 # Apply a dense layer to the every temporal slice of an input. For each of step
 # of the output sequence, decide which character should be chosen.
-model.add(layers.TimeDistributed(layers.Dense(len(chars))))
+# model.add(layers.TimeDistributed(layers.Dense(len(chars))))
+model.add(layers.Dense(len(chars)))
 model.add(layers.Activation('softmax'))
 # ref. http://dev.likejazz.com/post/175034937891/multiclass-classification%EC%97%90%EC%84%9C%EC%9D%98-binary-crossentropy
 # from keras.metrics import categorical_accuracy
